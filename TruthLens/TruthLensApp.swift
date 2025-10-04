feb556938df58427bbb9ba4f136a522da9133d43
shayan <shayan@vloom.ai>
2025-10-04 16:05:38 -0700

    Initial Commit

---
+++ TruthLens/TruthLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruthLensApp.swift
+//  TruthLens
+//
+//  Created by Shayan Sharvini on 2025-10-04.
+//
+
+import SwiftUI
+
+@main
+struct TruthLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TruthLensTests/TruthLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  TruthLensTests.swift
+//  TruthLensTests
+//
+//  Created by Shayan Sharvini on 2025-10-04.
+//
+
+import Testing
+@testable import TruthLens
+
+struct TruthLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
